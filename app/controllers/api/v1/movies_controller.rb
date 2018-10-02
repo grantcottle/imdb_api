71683e9158f8aa3ee64a201eec0228626b30c550
Grant Cottle <grantmaster89@msn.com>
2018-10-02 16:49:29 -0700

    fixing controller

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -10,38 +10,38 @@ class Api::V1::MoviesController < ApplicationController
 
   # GET /api/v1/movies/1
   def show
-    render json: @api_v1_movie
+    render json: @api_v1_movies
   end
 
   # POST /api/v1/movies
   def create
-    @api_v1_movie = Movie.new(api_v1_movie_params)
+    @api_v1_movies = Movie.new(api_v1_movie_params)
 
-    if @api_v1_movie.save
-      render json: @api_v1_movie, status: :created, location: @api_v1_movie
+    if @api_v1_movies.save
+      render json: @api_v1_movies, status: :created, location: @api_v1_movies
     else
-      render json: @api_v1_movie.errors, status: :unprocessable_entity
+      render json: @api_v1_movies.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /api/v1/movies/1
   def update
-    if @api_v1_movie.update(api_v1_movie_params)
-      render json: @api_v1_movie
+    if @api_v1_movies.update(api_v1_movie_params)
+      render json: @api_v1_movies
     else
-      render json: @api_v1_movie.errors, status: :unprocessable_entity
+      render json: @api_v1_movies.errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /api/v1/movies/1
   def destroy
-    @api_v1_movie.destroy
+    @api_v1_movies.destroy
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_api_v1_movie
-      @api_v1_movie = Api::V1::Movie.find(params[:id])
+      @api_v1_movies = Movie.find(params[:id])
     end
 
     # Only allow a trusted parameter "white list" through.
--- app/controllers/api/v1/shows_controller.rb
+++ app/controllers/api/v1/shows_controller.rb
@@ -10,38 +10,38 @@ class Api::V1::ShowsController < ApplicationController
 
   # GET /api/v1/shows/1
   def show
-    render json: @api_v1_show
+    render json: @api_v1_shows
   end
 
   # POST /api/v1/shows
   def create
-    @api_v1_show = Show.new(api_v1_show_params)
+    @api_v1_shows = Show.new(api_v1_show_params)
 
-    if @api_v1_show.save
-      render json: @api_v1_show, status: :created, location: @api_v1_show
+    if @api_v1_shows.save
+      render json: @api_v1_shows, status: :created, location: @api_v1_shows
     else
-      render json: @api_v1_show.errors, status: :unprocessable_entity
+      render json: @api_v1_shows.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /api/v1/shows/1
   def update
-    if @api_v1_show.update(api_v1_show_params)
-      render json: @api_v1_show
+    if @api_v1_shows.update(api_v1_show_params)
+      render json: @api_v1_shows
     else
-      render json: @api_v1_show.errors, status: :unprocessable_entity
+      render json: @api_v1_shows.errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /api/v1/shows/1
   def destroy
-    @api_v1_show.destroy
+    @api_v1_shows.destroy
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_api_v1_show
-      @api_v1_show = Api::V1::Show.find(params[:id])
+      @api_v1_shows = Show.find(params[:id])
     end
 
     # Only allow a trusted parameter "white list" through.
