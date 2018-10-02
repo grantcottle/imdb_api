60f76f0b6e5cf0c09eece4cba1812ca9b70397d1
Grant Cottle <grantmaster89@msn.com>
2018-10-02 16:36:36 -0700

    fixing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :api, :path => "" do
-    namespace :v1 :path => "" do
+    namespace :v1, :path => "" do
       resources :movies
       resources :shows
       get '/search' => 'search#search', :as => 'search'
